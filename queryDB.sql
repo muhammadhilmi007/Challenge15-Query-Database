31b1e6c59da0671cdea6c6cc4dfcacd261e4ac04
muhammadhilmiarsyad007 <muhammadhilmiarsyad@gmail.com>
2024-11-09 16:09:06 +0700

    Fix Task 1, 3 dan 5

--- queryDB.sql
+++ queryDB.sql
@@ -1,6 +1,13 @@
 -- Tambahkan column umur di table mahasiswa
 ALTER TABLE mahasiswa
-ADD COLUMN umur INTEGER;
+ADD COLUMN umur DATE;
+
+-- Tambahkan data umur di table mahasiswa
+UPDATE mahasiswa
+SET
+    umur = '2002-01-01'
+WHERE
+    nim = 2024001;
 
 -- Tampilkan Seluruh Data mahasiswa beserta nama jurusannya
 SELECT
@@ -18,15 +25,20 @@ SELECT
 FROM
     mahasiswa
 WHERE
-    umur < 20;
+    (strftime ('%Y', 'now') - strftime ('%Y', umur)) < 20;
 
 -- Tampilkan mahasiswa yang memiliki nilai 'B' ke atas
 SELECT
-    *
+    mahasiswa.nim,
+    mahasiswa.nama,
+    matakuliah.nama,
+    mahasiswa_matakuliah.nilai
 FROM
-    mahasiswa_matakuliah
+    mahasiswa
+    JOIN mahasiswa_matakuliah ON mahasiswa.nim = mahasiswa_matakuliah.nim_mahasiswa
+    JOIN matakuliah ON mahasiswa_matakuliah.id_matakuliah = matakuliah.id
 WHERE
-    nilai <= 'B';
+    mahasiswa_matakuliah.nilai IN ('A', 'B');
 
 -- Tampilkan mahasiswa yang memiliki sks lebih dari 10
 SELECT
@@ -43,13 +55,17 @@ GROUP BY
 HAVING
     SUM(matakuliah.sks) > 10;
 
--- Tampilkan mahasiswa yang mengambil mata kuliah 'Algoritma'
+-- Tampilkan mahasiswa yang mengambil mata kuliah 'Data Mining'
 SELECT
-    *
+    mahasiswa.nim,
+    mahasiswa.nama,
+    matakuliah.nama AS nama_matakuliah
 FROM
-    mahasiswa_matakuliah
+    mahasiswa
+    JOIN mahasiswa_matakuliah ON mahasiswa.nim = mahasiswa_matakuliah.nim_mahasiswa
+    JOIN matakuliah ON mahasiswa_matakuliah.id_matakuliah = matakuliah.id
 WHERE
-    id_matakuliah = 3;
+    matakuliah.nama = 'Data Mining';
 
 -- Tampilkan jumlah mahasiswa untuk setiap dosen
 SELECT
@@ -60,7 +76,8 @@ FROM
     JOIN dosen_matakuliah ON dosen.id = dosen_matakuliah.id_dosen
     JOIN mahasiswa_matakuliah ON dosen_matakuliah.id_matakuliah = mahasiswa_matakuliah.id_matakuliah
 GROUP BY
-    dosen.id, dosen.nama;
+    dosen.id,
+    dosen.nama;
 
 -- urutkan mahasiswa berdasarkan umurnya
 SELECT
@@ -72,7 +89,7 @@ ORDER BY
 
 -- tampilkan kontrak matakuliah yang harus diulangi(nilai D dan E), serta tampilkan data mahasiswa jurusan dan dosen secara lengkap. gunakan mode JOIN dan WHERE clause (solusi terdiri dari 2 sytanx SQL)
 -- Query 1: Menggunakan JOIN
-SELECT 
+SELECT
     m.nim,
     m.nama AS nama_mahasiswa,
     m.alamat,
@@ -81,18 +98,18 @@ SELECT
     mk.nama AS nama_matakuliah,
     d.nama AS nama_dosen,
     mm.nilai
-FROM 
+FROM
     mahasiswa m
     JOIN jurusan j ON m.id_jurusan = j.id
     JOIN mahasiswa_matakuliah mm ON m.nim = mm.nim_mahasiswa
     JOIN matakuliah mk ON mm.id_matakuliah = mk.id
     JOIN dosen_matakuliah dm ON mk.id = dm.id_matakuliah
     JOIN dosen d ON dm.id_dosen = d.id
-WHERE 
+WHERE
     mm.nilai IN ('D', 'E');
 
 -- Query 2: Menggunakan WHERE clause
-SELECT 
+SELECT
     m.nim,
     m.nama AS nama_mahasiswa,
     m.alamat,
@@ -101,18 +118,17 @@ SELECT
     mk.nama AS nama_matakuliah,
     d.nama AS nama_dosen,
     mm.nilai
-FROM 
+FROM
     mahasiswa m,
     jurusan j,
     mahasiswa_matakuliah mm,
     matakuliah mk,
     dosen_matakuliah dm,
     dosen d
-WHERE 
+WHERE
     m.id_jurusan = j.id
     AND m.nim = mm.nim_mahasiswa
     AND mm.id_matakuliah = mk.id
     AND mk.id = dm.id_matakuliah
     AND dm.id_dosen = d.id
     AND mm.nilai IN ('D', 'E');
-
